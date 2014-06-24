7d03f98b5257665273ff8281754761fbd87f9115
qiujz <qiujz@hotmail.com>
2014-06-24 22:29:38 +0800

    easyui

---
+++ AL.Backstage.Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AL.Backstage.Controllers
+{
+    public class NewsController:BaseController
+    {
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
