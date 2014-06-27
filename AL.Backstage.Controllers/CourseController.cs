f8aab740a3e559cf24dae433aa93ec0aefa54235
qjz07254 <qjz07254@17u.cn>
2014-06-27 17:18:57 +0800

    大文件上传

---
+++ AL.Backstage.Controllers/CourseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AL.Backstage.Controllers
+{
+    public class CourseController:BaseController
+    {
+        //<form action="/home/uploadfiles" method="post" enctype="multipart/form-data">
+        //    <label for="file">Filename:</label>
+        //    <input type="file" name="file" id="file" />
+ 
+        //    <input type="submit" name="submit" value="Submit" />
+        //</form>
+
+        public ActionResult UploadFiles()
+        {
+            foreach (string file in Request.Files)
+            {
+                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+                if (hpf.ContentLength == 0)
+                    continue;
+                string savedFileName = Path.Combine(
+                   AppDomain.CurrentDomain.BaseDirectory,
+                   Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+            }
+            return View("UploadedFiles");
+        }
+    }
+}
