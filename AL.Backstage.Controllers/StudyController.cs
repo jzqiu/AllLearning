ea2048cfdecd8317ae276cd9a141517b43f731ca
qiujz <qiujz@hotmail.com>
2014-06-29 12:47:26 +0800

    用户管理

---
+++ AL.Backstage.Controllers/StudyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AL.Backstage.Controllers
+{
+    public class StudyController:BaseController
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ AL.Backstage.Controllers/SystemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AL.Backstage.Controllers
+{
+    public class SystemController:BaseController
+    {
+        public ActionResult MUser()
+        {
+            return View();
+        }
+
+        public ActionResult MEditor()
+        {
+            return View();
+        }
+
+        public ActionResult LoginLog()
+        {
+            return View();
+        }
+
+        public ActionResult OprationLog()
+        {
+            return View();
+        }
+    }
+}
